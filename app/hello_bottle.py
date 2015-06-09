20e6ad02f9869095f4a536f056d7c9fcb399c83e
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-06-09 18:20:38 -0500

    Bottle Framework: Using Cookies

--- app/hello_bottle.py
+++ app/hello_bottle.py
@@ -12,6 +12,13 @@ def favorite_fruit():
     fruit = bottle.request.forms.get("fruit")
     if(fruit == None or fruit == ""):
         fruit = "No fruit selected"
+    bottle.response.set_cookie('fruit',fruit)
+    bottle.redirect('/show_fruit')
+    # return bottle.template('fruit_selection',{'fruit':fruit})
+
+@bottle.route('/show_fruit')
+def show_fruit():
+    fruit = bottle.request.get_cookie("fruit")
 
     return bottle.template('fruit_selection',{'fruit':fruit})
 
