3a20caaf78f323a719807bc5f145d3935c2a61b5
Omar Eduardo <torres.omar.eduardo@gmail.com>
2015-06-11 15:33:35 -0500

    Add Homework 1.3

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Homework 1.1
 mongorestore -h localhost:[forwarded_port] [path_to_your_dump]
 ***
 Homework 1.2
-Add hw1-2.py code into a bottle route or open python console and run with normal connection
+Add hw1-2.py code into a bottle route or open python console and run with normal connection (You must have pymongo installed in client)
+***
+Homework 1.3
+Add hw1-3.py code into a bottle route.
--- app/hello_bottle.py
+++ app/hello_bottle.py
@@ -7,6 +7,8 @@ def home_page():
     mythings = ['apple','orange','banana','peach']
     return bottle.template('home',username="Omar Torres",things=mythings)
 
+
+
 @bottle.post('/favorite_fruit')
 def favorite_fruit():
     fruit = bottle.request.forms.get("fruit")
@@ -31,6 +33,39 @@ def index(name):
     item = name.find_one()
     print "!"
     return '<b>Hello hey %s!</b>' % item['name']
+
+@bottle.route('/homeworks/1.2')
+def homework_1_2():
+    db_add = os.environ['DB_1_PORT_27017_TCP_ADDR']
+    connection = pymongo.MongoClient(db_add, 27017)
+    db = connection.m101
+    collection = db.funnynumbers
+    magic = 0
+    try:
+        iter = collection.find()
+        for item in iter:
+            if((item['value'] % 3) == 0):
+                magic = magic + item['value']
+    except Exception as e:
+        print "Error trying to read collection:", type(e),e
+
+    return "The answer to Homework One, Problem 2 is " + str(int(magic))
+
+@bottle.route('/homeworks/1.3/<n>')
+def homework_1_3(n):
+    db_add = os.environ['DB_1_PORT_27017_TCP_ADDR']
+    connection = pymongo.MongoClient(db_add, 27017)
+    db = connection.m101
+    collection = db.funnynumbers
+    n = int(n)
+    magic = 0
+    try:
+        iter = collection.find({},limit=1, skip=n).sort('value', direction=1)
+        for item in iter:
+            return str(int(item['value'])) + "\n"
+    except Exception as e:
+        return "HEE"
+        print "Error trying to read collection:", type(e), e
 
 bottle.debug(True)
 bottle.run(host='0.0.0.0', port=8000)
